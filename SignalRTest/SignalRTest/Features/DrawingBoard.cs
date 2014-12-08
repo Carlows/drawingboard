ad39d0a87ad97f245fdc1c861961b6ffbc4c47db
Carlos Martinez <carlosjesus94@gmail.com>
2014-12-08 18:54:11 -0430

    first commit

---
+++ SignalRTest/SignalRTest/Features/DrawingBoard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRTest.Features
+{
+    public class DrawingBoard : Hub
+    {
+        public Task StartBroadcastPoint(int x, int y)
+        {
+            return Clients.Others.StartPoint(x, y);
+        }
+
+        public Task BroadcastPoint(int x, int y)
+        {
+            string color = Clients.Caller.color;
+            return Clients.Others.DrawPoint(x, y, color);
+        }
+
+        public Task BroadcastClear()
+        {
+            return Clients.Others.Clear();
+        }
+    }
+}
